600e765a8611a5d18c8d04deccaedd22f281e808
S-o-b-u <138811082+S-o-b-u@users.noreply.github.com>
2023-07-31 00:15:30 +0530

    Add files via upload

---
+++ prime range.c
@@ -0,0 +1,27 @@
+//QUESTION 11
+#include<stdio.h>
+//Write a program to print prime numbers in a range.
+
+int isPrime(int n);
+
+void main() {
+	int n1, n2;
+	printf("enter n1 &n2\t");
+	scanf("%d %d", &n1, &n2);
+	printf("prime range between n1 & n2 :\n");
+	for(int i=n1; i<=n2; i++){
+		if(isPrime(i)){
+			printf("%d\t", i);
+		}
+	}
+}
+
+int isPrime(int n){
+	for(int i=2; i<=n/2; i++){
+		if(n%i==0){
+			return 0;
+		}if(n%i!=0){
+			return 1;
+		}
+	}
+}
