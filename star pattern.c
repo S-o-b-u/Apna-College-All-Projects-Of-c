8cb9689819b54a58bee2186b908075a26eeefa73
S-o-b-u <138811082+S-o-b-u@users.noreply.github.com>
2023-07-31 00:18:14 +0530

    Add files via upload

---
+++ star pattern.c
@@ -0,0 +1,22 @@
+//QUESTION 9
+#include<stdio.h>
+/* 
+a. Search on what a "nested loop" is & print this
+pattern using it.
+
+                *****
+                *****
+                *****
+                *****
+
+*/
+void main()
+{
+	int i, j;
+	for(i=0; i<4; i++){
+		for(j=0; j<5; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
