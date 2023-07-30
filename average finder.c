2c46921a6bceef095da9a1c6770ca5cdeb4230be
S-o-b-u <138811082+S-o-b-u@users.noreply.github.com>
2023-07-31 00:05:07 +0530

    Add files via upload

---
+++ average finder.c
@@ -0,0 +1,16 @@
+//QUESTION 3
+//Write comments for programs a & b.
+//that is totally BAWASIR;
+
+//HOMEWORK SHEET 2
+//QUESTION 4
+#include <stdio.h>
+//Write a program to print the average of 3 numbers.
+void main() 
+{
+	int a, b, c;
+	printf("enter a, b & c :\n");
+	scanf("%d %d %d", &a, &b, &c);
+	printf("total average is : %d", (a+b+c)/2);
+	
+}
