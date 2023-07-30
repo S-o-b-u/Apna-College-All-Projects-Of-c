f40ebeed22c3a976cb508d46650d3bd3a5e2b8a4
S-o-b-u <138811082+S-o-b-u@users.noreply.github.com>
2023-07-31 00:08:26 +0530

    Add files via upload

---
+++ cube finder.c
@@ -0,0 +1,11 @@
+//QUESTION 2
+#include <stdio.h>
+//Take a number(n) from user & output its cube(n*n*n).
+void main() 
+{
+	int n;
+	printf("enter number\n");
+	scanf("%d", &n);
+	
+	printf("cube value is : %d", n*n*n);
+}
